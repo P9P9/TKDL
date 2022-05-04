cdd2bd900f7795dcb6938bb261dceec0d886cd3c
Unknown <98932323+P9P9@users.noreply.github.com>
2022-05-04 13:58:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,6 +60,7 @@ async def _callbacks(bot, cb: CallbackQuery):
     cbb = cb
     update = cbb.message.reply_to_message
     await cb.message.delete()
+    await cb.send_message("downloaded with @TKDL2_BOT")
     url = update.text
     session = requests.Session()
     resp = session.head(url, allow_redirects=True)
